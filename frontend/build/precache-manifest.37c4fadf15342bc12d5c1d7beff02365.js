65c5748883e154096eb8c49961f9d9f25855b05b
SubhodeepSeal <39885016+SubhodeepSeal@users.noreply.github.com>
2022-08-04 18:57:51 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.37c4fadf15342bc12d5c1d7beff02365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda5f69b61aefd1ec6730f90482bb732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ca9d1ce0f40fdea6bc",
+    "url": "/static/css/main.1fa00b34.chunk.css"
+  },
+  {
+    "revision": "602ca9f32b83a4e5b340",
+    "url": "/static/js/2.c5442e1a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c5442e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ca9d1ce0f40fdea6bc",
+    "url": "/static/js/main.ef2b0fc3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
